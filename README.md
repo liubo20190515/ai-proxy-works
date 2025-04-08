645a988b49751866fa43b5f0dd9d0e5a8f022251
liubo20190515 <liubo20190515@gmail.com>
2025-04-07 20:43:33 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# ai-proxy-works
+# AI Proxy
+
+This is a simple proxy for AI services.
+
+## Sponsorship
+
+This project is sponsored by [ChatWise](https://chatwise.app), the fastest AI chatbot that works for any LLM.
+
+## Usage
+
+Replace your API domain with the domain of the proxy deployed on your server. For example:
+
+- Gemini:
+  - from `https://generativelanguage.googleapis.com/v1beta` 
+  - to`https://your-proxy/generativelanguage/v1beta`
+- OpenAI:
+  - from `https://api.openai.com/v1`
+  - to `https://your-proxy/openai/v1`
+- Anthropic:
+  - from `https://api.anthropic.com/v1`
+  - to `https://your-proxy/anthropic/v1`
+- Groq:
+  - from `https://api.groq.com/openai/v1`
+  - to `https://your-proxy/groq/openai/v1`
+- Perplexity:
+  - from `https://api.perplexity.ai`
+  - to `https://your-proxy/pplx`
+- Mistral:
+  - from `https://api.mistral.ai`
+  - to `https://your-proxy/mistral`
+- OpenRouter:
+  - from `https://openrouter.ai/api`
+  - to `https://your-proxy/openrouter`
+- xAI:
+  - from `https://api.xai.ai`
+  - to `https://your-proxy/xai`
+ 
+## Hosted by ChatWise
+
+Use the hosted API, for example OpenAI `https://ai-proxy.chatwise.app/openai/v1`
+
+## Deployment
+
+Deploy this as a Docker container, check out [Dockerfile](./Dockerfile).
+
+## License
+
+MIT.
---
+++ main.ts
@@ -0,0 +1,163 @@
+import { Hono } from "hono"
+import type { Context, Next } from "hono"
+import { cors } from "hono/cors"
+import { zValidator } from "@hono/zod-validator"
+import { z } from "zod"
+import { logger } from "hono/logger"
+import { proxy } from 'hono/proxy'
+
+
+const app = new Hono()
+
+const CustomProxyQuerySchema = z.object({
+  url: z.string().url(),
+})
+
+app.use(cors())
+
+app.use(logger())
+
+app.use(async (c: Context, next: Next) => {
+  await next()
+  c.res.headers.set("X-Accel-Buffering", "no")
+})
+
+app.get("/", (c: Context) => c.text("A proxy for AI!"))
+
+const fetchWithTimeout = async (
+  url: string,
+  { timeout, ...options }: RequestInit & { timeout: number },
+) => {
+  const controller = new AbortController()
+
+  const timeoutId = setTimeout(() => {
+    controller.abort()
+  }, timeout)
+
+  try {
+    const res = await proxy(url, {
+      ...options,
+      signal: controller.signal,
+      // @ts-expect-error
+      duplex: "half",
+    })
+    clearTimeout(timeoutId)
+    return res
+  } catch (error) {
+    clearTimeout(timeoutId)
+    if (controller.signal.aborted) {
+      return new Response("Request timeout", {
+        status: 504,
+      })
+    }
+
+    throw error
+  }
+}
+
+const proxies: { pathSegment: string; target: string; orHostname?: string }[] =
+  [
+    {
+      pathSegment: "generativelanguage",
+      orHostname: "gooai.chatkit.app",
+      target: "https://generativelanguage.googleapis.com",
+    },
+    {
+      pathSegment: "groq",
+      target: "https://api.groq.com",
+    },
+    {
+      pathSegment: "anthropic",
+      target: "https://api.anthropic.com",
+    },
+    {
+      pathSegment: "pplx",
+      target: "https://api.perplexity.ai",
+    },
+    {
+      pathSegment: "openai",
+      target: "https://api.openai.com",
+    },
+    {
+      pathSegment: "mistral",
+      target: "https://api.mistral.ai",
+    },
+    {
+      pathSegment: "openrouter/api",
+      target: "https://openrouter.ai/api",
+    },
+    {
+      pathSegment: "openrouter",
+      target: "https://openrouter.ai/api",
+    },
+    {
+      pathSegment: "xai",
+      target: "https://api.x.ai",
+    },
+  ]
+
+app.post(
+  "/custom-model-proxy",
+  zValidator(
+    "query",
+    CustomProxyQuerySchema,
+  ),
+  async (c) => {
+    const queryData = c.req.valid("query")
+    const url = queryData.url
+
+    const res = await proxy(url, {
+      method: c.req.method,
+      body: c.req.raw.body,
+      headers: c.req.raw.headers,
+    })
+
+    return new Response(res.body, {
+      headers: res.headers,
+      status: res.status,
+    })
+  },
+)
+
+app.use(async (c: Context, next: Next) => {
+  const url = new URL(c.req.url)
+
+  const proxy = proxies.find(
+    (p) =>
+      url.pathname.startsWith(`/${p.pathSegment}/`) ||
+      (p.orHostname && url.hostname === p.orHostname),
+  )
+
+  if (proxy) {
+    const headers = new Headers(c.req.raw.headers)
+    if (proxy.pathSegment === "anthropic") {
+      headers.delete("origin")
+    }
+    headers.delete('content-length')
+    headers.delete('host')
+
+    const res = await fetchWithTimeout(
+      `${proxy.target}${url.pathname.replace(
+        `/${proxy.pathSegment}/`,
+        "/",
+      )}${url.search}`,
+      {
+        method: c.req.method,
+        headers,
+        body: c.req.raw.body,
+        timeout: 60000,
+      },
+    )
+
+    return new Response(res.body, {
+      headers: res.headers,
+      status: res.status,
+    })
+  }
+
+  next()
+})
+
+export default {
+  fetch: app.fetch,
+}
